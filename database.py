3e3c65f4f4299a520576f28bc9fe9dfbec3ba211
Joanna <joanna@joanna-aspire.(none)>
2013-12-29 13:57:21 +0100

    Dodana tabela Loginy i metoda zwracająca hasło użytkownika.

--- database.py
+++ database.py
@@ -6,10 +6,10 @@ from torndb import Connection as MysqlConnection
 class DatabaseManager(MysqlConnection):
     
 	CREATE_TABLE_UCZNIOWIE = """CREATE TABLE IF NOT EXISTS Uczniowie (
-		id INT,
+		id INT AUTO_INCREMENT,
 		imie VARCHAR(32),
 		nazwisko VARCHAR(32),
-		pesel VARCHAR(16),
+		pesel VARCHAR(16) UNIQUE,
 		klasaId INT,
 		dataUrodzenia DATE,
 		adres VARCHAR(128),
@@ -21,32 +21,35 @@ class DatabaseManager(MysqlConnection):
 		
 		PRIMARY KEY (id),
 		FOREIGN KEY (klasaId) REFERENCES Klasy(id)
-	);
+	) collate utf8_general_ci;
 	"""
+	
 	CREATE_TABLE_KLASY = """CREATE TABLE IF NOT EXISTS Klasy (
-		id INT,
+		id INT AUTO_INCREMENT,
 		nazwa VARCHAR(64),
 		wychowawcaId INT,
 		
 		PRIMARY KEY (id),
 		FOREIGN KEY (wychowawcaId) REFERENCES Nauczyciele(id)
-	);
+	) collate utf8_general_ci;
 	"""
+	
 	CREATE_TABLE_NAUCZYCIELE = """CREATE TABLE IF NOT EXISTS Nauczyciele (
-		id INT,
+		id INT AUTO_INCREMENT,
 		imie VARCHAR(32),
 		nazwisko VARCHAR(32),
-		pesel VARCHAR(16),
+		pesel VARCHAR(16) UNIQUE,
 		dataUrodzenia DATE,
 		dataZatrudnienia DATE,
 		adres VARCHAR(128),
 		telefon VARCHAR(16),
 		
 		PRIMARY KEY (id)
-	);
+	) collate utf8_general_ci;
 	"""
+	
 	CREATE_TABLE_PRZEDMIOTY = """CREATE TABLE IF NOT EXISTS Przedmioty (
-		id INT,
+		id INT AUTO_INCREMENT,
 		nazwa VARCHAR(32),
 		nauczycielId INT,
 		klasaId INT,
@@ -54,10 +57,11 @@ class DatabaseManager(MysqlConnection):
 		PRIMARY KEY (id),
 		FOREIGN KEY (nauczycielId) REFERENCES Nauczyciele(id),
 		FOREIGN KEY (klasaId) REFERENCES Klasy(id)
-	);
+	) collate utf8_general_ci;
 	"""
+	
 	CREATE_TABLE_LEKCJE = """CREATE TABLE IF NOT EXISTS Lekcje (
-		id INT,
+		id INT AUTO_INCREMENT,
 		przedmiotId INT,
 		sala VARCHAR(32),
 		dzien INT,
@@ -65,11 +69,11 @@ class DatabaseManager(MysqlConnection):
 		
 		PRIMARY KEY (id),
 		FOREIGN KEY (przedmiotId) REFERENCES Przedmioty(id)
-	);
+	) collate utf8_general_ci;
 	"""
-
+	
 	CREATE_TABLE_OCENY = """CREATE TABLE IF NOT EXISTS Oceny (
-		id INT,
+		id INT AUTO_INCREMENT,
 		uczenId INT,
 		przedmiotId INT,
 		ocena INT,
@@ -79,7 +83,17 @@ class DatabaseManager(MysqlConnection):
 		PRIMARY KEY (id),
 		FOREIGN KEY (uczenId) REFERENCES Uczniowie(id),
 		FOREIGN KEY (przedmiotId) REFERENCES Przedmioty(id)
-	);
+	) collate utf8_general_ci;
+	"""
+	
+	CREATE_TABLE_LOGINY = """CREATE TABLE IF NOT EXISTS Loginy (
+		id INT AUTO_INCREMENT,
+		login VARCHAR(16),
+		haslo VARCHAR(48),
+		typ ENUM("UCZEN", "NAUCZYCIEL"),
+		
+		PRIMARY KEY (id)
+	) collate utf8_general_ci;
 	"""
 	
 	def __init__(self, host, user, password, database):
@@ -89,7 +103,7 @@ class DatabaseManager(MysqlConnection):
 		self.__create_tables()
     
 	def __create_tables(self):
-	
+		self.execute(DatabaseManager.CREATE_TABLE_LOGINY)
 		self.execute(DatabaseManager.CREATE_TABLE_NAUCZYCIELE)
 		self.execute(DatabaseManager.CREATE_TABLE_KLASY)
 		self.execute(DatabaseManager.CREATE_TABLE_UCZNIOWIE)
@@ -97,6 +111,15 @@ class DatabaseManager(MysqlConnection):
 		self.execute(DatabaseManager.CREATE_TABLE_LEKCJE)
 		self.execute(DatabaseManager.CREATE_TABLE_OCENY)
 
+	### dla kazdego:
+
+	def get_password(self, login, type):
+		query = """SELECT haslo FROM Loginy WHERE login = {} AND typ = {}""".format(login,type)
+		result = self.query(query)
+		if result is not None:
+			return result[0]
+		raise ValueError("Uzytkownik nie istnieje ({}, {})".format(login, type))
+		
 	### dla ucznia:
 	
 	def get_user_schedule(self, userId):
