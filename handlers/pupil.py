10a50aa51c08a79dd34bf553cdc58170a341ba53
robin92pl <robin92pl@gmail.com>
2013-12-29 23:05:42 +0100

    Wprowadzono dekorator require_pupil.
    Dekorator wymaga by osoba była zalogowana jako UCZEN.

--- handlers/pupil.py
+++ handlers/pupil.py
@@ -3,19 +3,30 @@
 # tornado framework
 from tornado.web import authenticated, RequestHandler, HTTPError
 
+# standardowe
+from functools import wraps as func_wraps
+
 # nasze
 from base import BaseHandler
+
+def require_pupil(method):
+    """ Decorate methods with this to require that the user be a pupil. """
+    
+    @func_wraps(method)
+    def wrapper(self, *args, **kwargs):
+        if not self.session or self.session["type"] != "UCZEN": raise HTTPError(403)
+        return method(self, *args, **kwargs)
+    
+    return wrapper
 
 class GradeHandler(BaseHandler):
 
     @authenticated
-    def get(self, courseStr):
-        session = self.get_session()
-        if session["type"] != "UCZEN": raise HTTPError(403)
-    
+    @require_pupil
+    def get(self, courseStr):    
         courseId = None
         if courseStr != "all": courseId = int(courseStr)      
 
-        grades = self.db.get_user_grades(session["userId"], courseId)
+        grades = self.db.get_user_grades(self.session["userId"], courseId)
         self.render("pupil/grades.html", grades = grades)
 
