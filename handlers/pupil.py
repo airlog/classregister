ff5656295b3400683d1e2a2258b9a9895e45b8b1
robin92pl <robin92pl@gmail.com>
2013-12-30 21:57:29 +0100

    Udoskonalono funkcję require_pupil.
    Funkcja przyjmuje teraz argument będący szablonem adresu URL przekierowania, jeśli aktywny użytkownik nie jest uczniem.
    Podany szablon URL oferuje zamianę ARG(\d+) na odpowiadający mu argumenty dekorowanej metody oraz zamianę SESSION_USER na wartość pola "user" z sesji.

--- handlers/pupil.py
+++ handlers/pupil.py
@@ -5,19 +5,24 @@ from tornado.web import authenticated, RequestHandler, HTTPError
 
 # standardowe
 from functools import wraps as func_wraps
+from re import compile as Regex
 
 # nasze
 from base import BaseHandler
 
-def require_pupil(method):
-    """ Decorate methods with this to require that the user be a pupil. """
-    
-    @func_wraps(method)
-    def wrapper(self, *args, **kwargs):
-        if not self.session or self.session["type"] != "UCZEN": raise HTTPError(403)
-        return method(self, *args, **kwargs)
-    
-    return wrapper
+def __parse_template(template, session, args):
+    for i in xrange(len(args)): template = template.replace("ARG{}".format(i), str(args[i]))
+    return template.replace("SESSION_USER", session["user"])
+
+def require_pupil(redirectUrl = "/"):
+    def actual_wrapper(method):
+        @func_wraps(method)
+        def wrapper(self, *args, **kwargs):
+            if not self.session or self.session["type"] != "UCZEN":
+                self.redirect(__parse_template(redirectUrl, self.session, args))
+            return method(self, *args, **kwargs)
+        return wrapper    
+    return actual_wrapper
 
 class MainHandler(BaseHandler):
     
@@ -26,7 +31,7 @@ class MainHandler(BaseHandler):
         elif self.session["user"] != pesel: raise HTTPError(403)    # PESEL w URL jest inny niż PESEL aktywnego użytkownika
         
     @authenticated
-    @require_pupil
+    @require_pupil("/teacher/SESSION_USER/pupil/ARG0")
     def get(self, pesel):
         self._validate_pesel(pesel)
         self.render("pupil/main.html")
@@ -34,7 +39,7 @@ class MainHandler(BaseHandler):
 class GradeHandler(MainHandler):
 
     @authenticated
-    @require_pupil
+    @require_pupil("/teacher/SESSION_USER/pupil/ARG0/grades")
     def get(self, pesel, courseStr):
         self._validate_pesel(pesel)
         
